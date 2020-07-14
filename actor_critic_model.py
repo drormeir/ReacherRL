82d52ee82c6dfdd2b862f2bc4f6b6501a6530938
Dror Meirovich <dror@drorm.com>
2020-07-14 10:24:17 +0300

    actor critic recieves network architecture as input

--- actor_critic_model.py
+++ actor_critic_model.py
@@ -4,85 +4,108 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-def hidden_init(layer):
-    fan_in = layer.weight.data.size()[0]
-    lim = 1. / np.sqrt(fan_in)
-    return (-lim, lim)
+
+class ModuleListUtil(nn.ModuleList):
+    def __init__(self, in_features = -1, layers_arch = [], out_features=0):
+        super(ModuleListUtil,self).__init__()
+        self.build(in_features=in_features, layers_arch=layers_arch, out_features=out_features)
+
+    def build(self, in_features, layers_arch, out_features=0):
+        self.out_features = in_features
+        self.layers_arch  = layers_arch.copy()
+        for layer in layers_arch:
+            if isinstance(layer, str):
+                if layer == 'b':
+                    self.append(nn.BatchNorm1d(self.out_features))
+                elif layer == 'r':
+                    self.append(nn.LeakyReLU())
+            elif isinstance(layer,int):
+                self.append_linear(out_features=layer)
+        if out_features > 0:
+            self.append_linear(out_features=out_features)
+
+    def append_linear(self, out_features):
+        # reference for nn.init.xavier_uniform_() can be found at: https://pytorch.org/docs/stable/nn.init.html`
+        linear = nn.Linear(in_features=self.out_features, out_features=out_features)
+        nn.init.xavier_uniform_(linear.weight)
+        self.append(linear)
+        self.out_features = out_features
+
+    def forward(self, x):
+        for layer in self:
+            x = layer(x)
+        return x
+
 
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=400, fc2_units=300, pytorch_device=None):
+    def __init__(self, state_size, action_size, layers_arch = [100,100,50], pytorch_device=None):
         """Initialize parameters and build model.
         Params
         ======
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
-            seed (int): Random seed
-            fc1_units (int): Number of nodes in first hidden layer
-            fc2_units (int): Number of nodes in second hidden layer
         """
         super(Actor, self).__init__()
-        self.seed   = torch.manual_seed(seed)
-        self.fc1    = nn.Linear(state_size, fc1_units)
-        self.fc2    = nn.Linear(fc1_units, fc2_units)
-        self.fc3    = nn.Linear(fc2_units, action_size)
-        self.reset_parameters()
-        self.device = pytorch_device
-        if self.device is not None:
-            self.to(self.device)
-
-    def reset_parameters(self):
-        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+        self.state_size     = state_size
+        self.action_size    = action_size
+        self.layers_list    = ModuleListUtil(in_features=self.state_size, layers_arch=layers_arch, out_features=self.action_size)
+        self.pytorch_device = pytorch_device
+        if self.pytorch_device is not None:
+            self.to(self.pytorch_device)
+    
+    def clone(self):
+        return Actor(state_size=self.state_size, action_size=self.action_size, layers_arch=self.layers_list.layers_arch.copy(), pytorch_device=self.pytorch_device)
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        return self.layers_list.forward(state)
 
     def eval_numpy(self, state):
+        if state.ndim == 1:
+            state = state.reshape((1,len(state)))
         state = torch.from_numpy(state).float()
-        if self.device:
-            state = state.to(self.device)
+        if self.pytorch_device:
+            state = state.to(self.pytorch_device)
         self.eval() # set model to "eval" mode
         with torch.no_grad():
             action = self(state).cpu().data.numpy()
+        self.train()
         return action
     
 class Critic(nn.Module):
     """Critic (Value) Model."""
 
-    def __init__(self, state_size, action_size, seed, fcs1_units=400, fc2_units=300, pytorch_device=None):
+    def __init__(self, state_size, action_size, layers_arch = [[100],[50],[50]], pytorch_device=None):
         """Initialize parameters and build model.
         Params
         ======
             state_size (int): Dimension of each state
             action_size (int): Dimension of each action
-            seed (int): Random seed
-            fcs1_units (int): Number of nodes in the first hidden layer
-            fc2_units (int): Number of nodes in the second hidden layer
         """
         super(Critic, self).__init__()
-        self.seed = torch.manual_seed(seed)
-        self.fcs1 = nn.Linear(state_size, fcs1_units)
-        self.fc2  = nn.Linear(fcs1_units+action_size, fc2_units)
-        self.fc3  = nn.Linear(fc2_units, 1)
-        self.reset_parameters()
+        self.state_size     = state_size
+        self.action_size    = action_size
+        self.pytorch_device = pytorch_device
+        self.state_layers   = ModuleListUtil(in_features=self.state_size,  layers_arch=layers_arch[0])
+        self.action_layers  = ModuleListUtil(in_features=self.action_size, layers_arch=layers_arch[1])
+        self.combine_layers = ModuleListUtil(in_features=self.state_layers.out_features + self.action_layers.out_features, layers_arch=layers_arch[2], out_features=1)
         if pytorch_device is not None:
             self.to(pytorch_device)
+        return
 
-    def reset_parameters(self):
-        self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
-        self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
-        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
+    def clone(self):
+        return Critic(state_size=self.state_size, action_size=self.action_size,\
+        layers_arch=[self.state_layers.layers_arch.copy(), self.action_layers.layers_arch.copy(), self.combine_layers.layers_arch.copy()],
+        pytorch_device=self.pytorch_device)
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
-        xs = F.relu(self.fcs1(state))
-        x  = torch.cat((xs, action), dim=1)
-        x  = F.relu(self.fc2(x))
-        return self.fc3(x)
+        xs = self.state_layers.forward(state)
+
+        xa = self.action_layers.forward(action)
+
+        x  = torch.cat((xs, xa), dim=1)
+
+        return self.combine_layers.forward(x)
