e64dd3a9149b5e1bb556b4d8bf89ee2715b9fd4d
Dror Meirovich <dror@drorm.com>
2020-07-17 21:10:28 +0300

    fix comments. refactoring training process. fix reward to be 0.1

--- unity_environment_wrapper.py
+++ unity_environment_wrapper.py
@@ -5,7 +5,7 @@ import shutil
 
 class unity_env(UnityEnvironment):
 
-    def __init__(self, file_name, ind_brain=0, no_graphics=False, verbose_level=1, max_steps_in_single_episode=1000, score_goal=30,\
+    def __init__(self, file_name, ind_brain=0, no_graphics=False, verbose_level=1, max_steps_in_single_episode=2000, score_goal=30,\
                  score_window_size=100):
         super().__init__(file_name=file_name, no_graphics=no_graphics)
         self.ind_brain     = ind_brain        
@@ -30,19 +30,30 @@ class unity_env(UnityEnvironment):
           
     
 
-    def train(self, agent, output_filename, n_episodes = 10000, score_window_size=None, noise_minimal=0.01, noise_decay=0.995):
-        """Deep Q-Learning.
+    def train(self, agent, output_filename, max_num_episodes = 10000, num_episode_search = 500, score_window_size=None, noise_minimal=0.01, noise_decay=0.99):
+        """DDPG learning.
 
         Params
         ======
-            env (banana_env): the environment
-            agent (ddqn_agent): the agent
+            agent (ddpg_agent):       the agent
             output_filename (string): file name for check point
-            n_episodes (int): maximum number of training episodes
-            eps_end (float): minimum value of epsilon-greedy action selection
-            eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+            max_num_episodes (int):   maximum number of training episodes
+            num_episode_search (int): maximum number of episode to search for better score
+            noise_minimal (float):    minimum value of noise multiplier
+            noise_decay (float):      multiplicative factor (per episode) for decreasing noise
         """
         self.reset_all_scores(score_window_size)
+        if num_episode_search is None or (num_episode_search < self.scores_window.maxlen):
+            num_episode_search = self.scores_window.maxlen
+        if self.verbose_level>0:
+            print("Training DDPG agent:")
+            print("output_filename    =",output_filename)
+            print("noise_minimal      =",noise_minimal)
+            print("noise_decay        =",noise_decay)
+            print("num_episode_search =",num_episode_search)
+            print("max_num_episodes   =",max_num_episodes)
+            print("score_window_size  =",self.scores_window.maxlen)
+
         agent.reset_noise_level()                    # initialize noise
         temp_checkpoint_name = "temp_checkpoint.pth"
         shutil.rmtree(temp_checkpoint_name,ignore_errors=True) # avoid file not found error
@@ -50,20 +61,36 @@ class unity_env(UnityEnvironment):
             curr_theta = agent.noise.theta
             curr_sigma = agent.noise.sigma
             curr_noise_level = agent.get_noise_level()
-            if self.train_agent_on_single_episode(agent):
-                if agent.get_noise_level() > noise_minimal:
-                    agent.scale_noise(noise_decay)
-            self.__update_statistics_with_completed_episode_score()
-            ind_episode = len(self.all_scores)
+            self.train_agent_on_single_episode(agent)
+            # decay noise only if new episode score is smaller than previous episode score
+            if len(self.all_scores) > 0 and self.episode_score < self.all_scores[-1] and agent.get_noise_level() > noise_minimal:
+                agent.scale_noise(noise_decay)
+            current_score_is_ok = self.episode_score > self.curr_window_average # compare value before updating window
+            self.scores_window.append(self.episode_score)
+            self.all_scores.append(self.episode_score)
+            self.__recalc_curr_window_score()
+            self.improvement  = 0 # train may continue
+            num_episodes  = len(self.all_scores)
+            if num_episodes >= self.scores_window.maxlen:
+                if self.curr_window_score > self.best_window_score and current_score_is_ok:
+                    # real score improvement :-)
+                    self.improvement = 1 # train should continue
+                    self.__set_curr_score_window_as_best()
+                elif self.curr_window_average > self.temp_window_average:
+                    # no real score improvement but still try a bit more...
+                    self.temp_window_average = self.curr_window_average
+                    self.best_window_episode = num_episodes
+                elif num_episodes >= self.best_window_episode + num_episode_search:
+                    # no score improvement anymore :-(
+                    self.improvement = -1 # train should stop
             if self.verbose_level > 1:
-                print('Episode {:4} score={:5.2f} noise:[level={:6.4f} theta={:6.4f} sigma={:6.4f} max={:5.2f}] curr window= {}'.format(ind_episode,\
-                self.total_score_in_episode, curr_noise_level, curr_theta, curr_sigma, agent.get_recent_episode_noise_max_amp(), self.str_curr_window_score()))
+                print('Episode {:3} --> score={:5.2f} curr window: {}'.format(num_episodes, self.episode_score, self.str_curr_window_score()))
             elif self.verbose_level > 0:
                 end         = "\n" if self.improvement > 0 else ""
-                print('\rEpisode {}\t {}'.format(ind_episode,self.str_curr_window_score()), end=end)
+                print('\rEpisode {}\t {}'.format(num_episodes,self.str_curr_window_score()), end=end)
             if self.__goal_reached_first_time():
-                print('\nEnvironment goal reached in {:d} episodes!'.format(ind_episode))
-            if ind_episode >= n_episodes:
+                print('\nEnvironment goal reached in {:d} episodes!'.format(num_episodes))
+            if num_episodes >= max_num_episodes:
                 # rare case: maximum number of episodes  -->  end training loop in any case
                 self.__test_improvement(agent, output_filename)
                 break
@@ -84,7 +111,7 @@ class unity_env(UnityEnvironment):
 
         shutil.rmtree(temp_checkpoint_name,ignore_errors=True) # avoid file not found error
         print('\nNo more improvements. End of training.')
-        return self.scores, self.best_test_score, self.best_test_average, self.best_test_stdev
+        return self.all_scores, self.best_test_score, self.best_test_average, self.best_test_stdev
 
     def reset_all_scores(self, score_window_size=None):
         if score_window_size is None:
@@ -109,7 +136,7 @@ class unity_env(UnityEnvironment):
             if done:
                 break
             state  = next_state
-        return agent.has_any_rewards
+        
     
     def test_agent_on_single_episode(self, agent, max_steps_in_single_episode=None, verbose=True):
         if verbose:
@@ -124,8 +151,8 @@ class unity_env(UnityEnvironment):
                 break
         if verbose:
             print("Agent: Test on single episode is over. Number of steps: {}  score: {:5.2f}"\
-                  .format(self.number_steps_in_episode, self.total_score_in_episode))
-        return self.total_score_in_episode
+                  .format(self.episode_length, self.episode_score))
+        return self.episode_score
     
     def test_random_agent_on_single_episode(self, agent, max_steps_in_single_episode=None, verbose=True):
         if verbose:
@@ -140,52 +167,54 @@ class unity_env(UnityEnvironment):
                 break
         if verbose:
             print("Agent: Test on single episode is over. Number of steps: {}  score: {:5.2f}"\
-                  .format(self.number_steps_in_episode, self.total_score_in_episode))
-        return self.total_score_in_episode
+                  .format(self.episode_length, self.episode_score))
+        return self.episode_score
     
-    def test_agent_on_many_episodes(self, agent, n_episodes=None, max_steps_in_single_episode=None, verbose=True):
-        if n_episodes is None:
-            n_episodes = self.scores_window.maxlen
+    def test_agent_on_many_episodes(self, agent, max_num_episodes=None, max_steps_in_single_episode=None, verbose=True):
+        if max_num_episodes is None:
+            max_num_episodes = self.scores_window.maxlen
         if verbose:
-            print("Begin test agent on {} episodes...".format(n_episodes))
+            print("Begin test agent on {} episodes...".format(max_num_episodes))
         if max_steps_in_single_episode is None:
             max_steps_in_single_episode = self.max_steps_in_single_episode
         scores = []
-        for i_episode in range(n_episodes):
+        for i_episode in range(max_num_episodes):
             state = self.reset(train_mode=True) # reset the environment
             for i_step in range(max_steps_in_single_episode):
                 action            = agent.act(state)     # select an action with eploitation only
                 state, _, done, _ = self.step(action)
                 if done:
                     break
-            scores.append(self.total_score_in_episode)
+            scores.append(self.episode_score)
             if verbose:
-                print("\rEpisode: {} out of: {} ended with score: {:5.2f}".format(i_episode+1,n_episodes,self.total_score_in_episode),end="")
+                print("\rEpisode: {} out of: {} ended with score: {:5.2f}".format(i_episode+1,max_num_episodes,self.episode_score),end="")
         scores_mean      = np.mean(scores)
         scores_stdev     = np.std(scores,ddof=1) if len(scores) > 1 else np.inf
         scores_composite = scores_mean - scores_stdev
         if verbose:
-            print("\nTest results on {} episodes: Composite={:5.2f} Average={:5.2f} Stdev={:5.2f}".format(n_episodes,scores_composite, scores_mean, scores_stdev))
+            print("\nTest results on {} episodes: Composite={:5.2f} Average={:5.2f} Stdev={:5.2f}".format(max_num_episodes,scores_composite, scores_mean, scores_stdev))
         return scores_composite, scores_mean, scores_stdev, scores
 
     def reset(self, train_mode):
-        env_info    = super().reset(train_mode=train_mode)[self.brain_name] # reset the environment
-        state       = env_info.vector_observations[self.ind_brain]          # get the current state
-        self.number_steps_in_episode = 0
-        self.total_score_in_episode  = 0
+        env_info            = super().reset(train_mode=train_mode)[self.brain_name] # reset the environment
+        state               = env_info.vector_observations[self.ind_brain]          # get the current state
+        self.episode_length = 0
+        self.episode_score  = 0
         return state
     
     def step(self, action):
-        env_info     = super().step(action)[self.brain_name]        # send the action to the environment
-        next_state   = env_info.vector_observations[self.ind_brain] # get the next state
-        reward       = env_info.rewards[self.ind_brain]             # get the reward
-        done         = env_info.local_done[self.ind_brain]          # see if episode has finished
-        self.number_steps_in_episode += 1
-        self.total_score_in_episode  += reward
+        env_info             = super().step(action)[self.brain_name]        # send the action to the environment
+        next_state           = env_info.vector_observations[self.ind_brain] # get the next state
+        reward               = env_info.rewards[self.ind_brain]             # get the reward
+        if reward > 1e-6:
+            reward = 0.1
+        done                 = env_info.local_done[self.ind_brain]          # see if episode has finished
+        self.episode_length += 1
+        self.episode_score  += reward
         return next_state, reward, done, env_info
 
     def str_curr_window_score(self):
-        return "Composite={:5.2f} Average={:5.2f} Stdev={:5.2f}".format(self.curr_window_score, self.curr_window_average, self.curr_window_stdev)
+        return "Average={:5.2f} Stdev={:5.2f} Composite={:5.2f}".format(self.curr_window_average, self.curr_window_stdev, self.curr_window_score)
 
     def __test_improvement(self, agent, output_filename):
         print("\nCompare last checkpoint against best test score so far...");
@@ -196,7 +225,7 @@ class unity_env(UnityEnvironment):
             while not done:
                 action            = agent.act(state)     # select an action
                 state, _, done, _ = self.step(action)
-            self.scores_window.append(self.total_score_in_episode)
+            self.scores_window.append(self.episode_score)
             print("\rEpisode: {} out of: {}".format(i_episode+1,score_window_size),end="")
         print("\n")
         self.__recalc_curr_window_score()
@@ -213,28 +242,6 @@ class unity_env(UnityEnvironment):
         agent.save(output_filename)
         return True
         
-    def __update_statistics_with_completed_episode_score(self):
-        completed_episode_score = self.total_score_in_episode
-        current_score_is_better_than_before = completed_episode_score > self.curr_window_average
-        self.scores_window.append(completed_episode_score)       # save most recent score
-        self.all_scores.append(completed_episode_score)          # save most recent score
-        self.__recalc_curr_window_score()
-        self.improvement  = 0 # train may continue
-        num_episodes  = len(self.all_scores)
-        if num_episodes < self.scores_window.maxlen:
-            return
-        if self.curr_window_score > self.best_window_score and current_score_is_better_than_before:
-            # real score improvement :-)
-            self.improvement = 1 # train should continue
-            self.__set_curr_score_window_as_best()
-        elif self.curr_window_average > self.temp_window_average:
-            # no real score improvement but still try a bit more...
-            self.temp_window_average = self.curr_window_average
-            self.best_window_episode = num_episodes
-        elif num_episodes >= self.best_window_episode + self.scores_window.maxlen:
-            # no score improvement anymore :-(
-            self.improvement = -1 # train should stop
-
     def __recalc_curr_window_score(self):
         curr_len = len(self.scores_window)
         self.curr_window_average = np.mean(self.scores_window) if curr_len > 0 else -np.inf
